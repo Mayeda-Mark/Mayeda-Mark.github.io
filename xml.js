f14d4b3a5191cc7e3e9331f69c969450325906cb
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-11 12:35:25 -0700

    added xmlhttp

---
+++ xml.js
@@ -0,0 +1,12 @@
+// xml.js
+
+function request() {
+	var xhttp = new XMLHttpRequest();
+	xttp.onreadystatechange = function() {
+		if(this.readyState ==4 && this.status == 200) {
+			document.getElementById("text").innerHTML = this.responseText;
+		}
+	};
+	xttp.open("GET", "test.txt", true);
+	xhttp.send();
+}
