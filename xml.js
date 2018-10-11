89caf64b0ad078202f5aa72f58ad42ba6bee673b
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-11 12:42:10 -0700

    try fix

--- xml.js
+++ xml.js
@@ -2,7 +2,7 @@
 
 function request() {
 	var xhttp = new XMLHttpRequest();
-	xttp.onreadystatechange = function() {
+	xhttp.onreadystatechange = function() {
 		if(this.readyState ==4 && this.status == 200) {
 			document.getElementById("text").innerHTML = this.responseText;
 		}
