62a08adaf02385f0cbb19d7b3d6d8b3d9adeca68
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-11 09:49:33 -0700

    started JSON

--- json.js
+++ json.js
@@ -6,12 +6,28 @@ function Team(name, wins, losses){
 	this.losses = losses;
 }
 
+var team1;
+var myJSON;
+
 function stringify() {
 	var name = document.getElementById('name').value;
 	var wins = document.getElementById('wins').value;
 	var losses = document.getElementById('losses').value;
 	var team1 = new Team(name, wins, losses);
 
-	var myJSON = JSON.stringify(team1);
-	document.getElementById('stringifyOutput').innerHTML = myJSON;
+	myJSON = JSON.stringify(team1);
+	document.getElementById('stringifyOutput').innerHTML = myJSON +
+	"<br><button onclick='parse();'>Click here to parse the JSON</button><div id='parseOutput'></div>";
+}
+
+function parse() {
+	var name = document.getElementById('name').value;
+	var wins = document.getElementById('wins').value;
+	var losses = document.getElementById('losses').value;
+	var team1 = new Team(name, wins, losses);
+	myJSON = JSON.stringify(team1);
+	var parse = JSON.parse(myJSON);
+	document.getElementById('parseOutput').innerHTML = "The " + parse.name 
+	+ " have a win/loss record of " + parse.wins + " win(s) and " + 
+	parse.losses + " loss(es).";
 }
