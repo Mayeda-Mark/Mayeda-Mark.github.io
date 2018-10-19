4540ab62d894565073f2a88f6a58853ee66d5af2
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-19 12:01:35 -0700

    added javacss

--- dommanip.js
+++ dommanip.js
@@ -2,7 +2,6 @@
 function generate1() {
 	var btn = document.createElement("BUTTON");
 	var t = document.createTextNode("Oh wait.  Click here");
-	//btn.onclick = generate2();
 	btn.onclick = function() {
 		var btn2 = document.createElement("BUTTON");
 		var t = document.createTextNode("No, no, no. Click Here");
@@ -14,7 +13,7 @@ function generate1() {
 				var t = document.createTextNode("I'm sorry. Click this one");
 				btn4.onclick = function() {
 					var para = document.createElement("P");
-					var text = document.createTextNode("Here is some text to reward your effores");
+					var text = document.createTextNode("Here is some text to reward your efforts");
 					para.appendChild(text);
 					document.getElementById('fill5').appendChild(para);
 					} // click 5
@@ -30,35 +29,3 @@ function generate1() {
 	btn.appendChild(t);
 	document.getElementById('fill1').appendChild(btn);
 }
-
-/*function generate2() {
-	var btn2 = document.createElement("BUTTON");
-	var t = document.createTextNode("No, no, no. Click Here");
-	btn2.onclick = generate3();
-	btn2.appendChild(t);
-	document.getElementById('fill2').appendChild(btn2);
-}
-
-function generate3() {
-	var btn3 = document.createElement("BUTTON");
-	var t = document.createTextNode("Shoot! No, click this one");
-	btn3.onclick = generate4();
-	btn3.appendChild(t);
-	document.getElementById('fill3').appendChild(btn3);
-}
-
-function generate4() {
-	var btn4 = document.createElement("BUTTON");
-	var t = document.createTextNode("I'm sorry. Click this one");
-	btn4.onclick = generate5();
-	btn4.appendChild(t);
-	document.getElementById('fill4').appendChild(btn4);
-}
-
-function generate5() {
-	var para = document.createElement("P");
-	var text = document.createTextNode("Here is some text to reward your effores");
-	para.appendChild(text);
-	document.getElementById('fill5').appendChild(para);
-}
-*/
---
+++ javacss.js
@@ -0,0 +1,6 @@
+//javacss.js
+
+function sentenceBackground(color) {
+	var bColor = "\"" + color + "\"";
+	document.getElementByClassName("class1").style.color = bColor;
+}
