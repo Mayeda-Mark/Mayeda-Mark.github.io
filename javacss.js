b9465bf05eebcd86cb6836f87cf0ae4df1275016
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-22 19:25:48 -0700

    added javacss files

--- javacss.js
+++ javacss.js
@@ -1,6 +1,27 @@
 //javacss.js
 
-function sentenceBackground(color) {
-	var bColor = "\"" + color + "\"";
-	document.getElementByClassName("class1").style.color = bColor;
+function sentenceBackground() {
+	var select_id = "select1";
+	var select = "document.getElementById(select_id);"
+
+	var class_name = "class1";
+	//var classes = document.getElementsByClassName(class_name);
+
+	var color = document.getElementById('select1').value;
+	for (var i = 0; i < document.getElementsByClassName(class_name).length; i++) {
+		document.getElementsByClassName(class_name)[i].style.backgroundColor =  color ;
+	}
+}
+
+function numbersBackground() {
+	var select_id = "select2";
+	var select = "document.getElementById(select_id);"
+
+	var class_name = "class2";
+	//var classes = document.getElementsByClassName(class_name);
+
+	var color = document.getElementById('select2').value;
+	for (var i = 0; i < document.getElementsByClassName(class_name).length; i++) {
+		document.getElementsByClassName(class_name)[i].style.backgroundColor =  color ;
+	}
 }
