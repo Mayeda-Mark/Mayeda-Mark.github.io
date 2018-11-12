54db10205539b638eccbb697e4b84b44cd02d526
Mayeda-Mark <markrmayeda@gmail.com>
2018-11-12 15:22:18 -0800

    updating xml

--- xml.js
+++ xml.js
@@ -2,11 +2,23 @@
 
 function request() {
 	var xhttp = new XMLHttpRequest();
+	var url = 'https://api.clashroyale.com/v1/cards';
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
-			document.getElementById("text").innerHTML = this.responseText;
+			var myResponse = JSON.parse(this.response);
+			var objectKeys = Object.keys(myResponse);
+			console.log('objectKeys: ', objectKeys);
+			var toWrite = "<br>";
+			for (var i = 0; i < objectKeys.length; i++) {
+				console.log("object number: " + i);
+				toWrite += (myResponse[i].name + "<br>")
+			}
+			document.getElementById("text").innerHTML = toWrite;
+		}
+		else {
+			document.getElementById("text").innerHTML = "File not found";
 		}
 	};
-	xhttp.open("GET", "test.txt", true);
+	xhttp.open("GET", url, true);
 	xhttp.send();
 }
