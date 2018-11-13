c80340b336e42c6177c0e5f244842a50006f772e
Mayeda-Mark <markrmayeda@gmail.com>
2018-11-12 16:40:50 -0800

    updated localstorage

--- localstorage.js
+++ localstorage.js
@@ -1,8 +1,20 @@
 //localstorage.js
 
 localStorage.setItem("card", "a card");
+//localStorage.setItem("name", "");
 
 function yourCard() {
 	localStorage.setItem("card", document.getElementById('card').value);
 	document.getElementById('yourCard').innerHTML = "Your card is the " + localStorage.getItem("card");
+}
+
+function setName() {
+	localStorage.setItem("name", document.getElementById('name').value);
+	document.getElementById('nameResult').innerHTML = "You name has been recorded.  Refresh page to see it."
+}
+
+function sayName() {
+	if (localStorage.name != "") {
+		document.getElementById('nameResult').innerHTML = "Hello " + localStorage.getItem("name");
+	}
 }
